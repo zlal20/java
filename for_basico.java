4dc3df9c567e124dd0c348c9ef9e1f0092956e18
Eduardo Franco <jesuseduardofrancomejia69@gmail.com>
2021-09-09 16:33:47 -0500

    Create for_basico.java

---
+++ for_basico.java
@@ -0,0 +1,14 @@
+public class for_basico {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 10; i++) {
+
+            // imprime los numeros del 0 al 9
+            System.out.println("los numeros son" + i);
+
+        }
+
+    }
+
+}

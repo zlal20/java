c620fab31b45f9b6417ef500129ecdc3b8d19a39
Eduardo Franco <jesuseduardofrancomejia69@gmail.com>
2021-09-09 16:02:02 -0500

    Update suma_switch.java

--- suma_switch.java
+++ suma_switch.java
@@ -6,6 +6,21 @@ public class suma_switch {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+        double numero1, numero2, resultado;
+
+        System.out.println("ingresa un numero del 1 al 2");
+        opcion = sc.nextInt();
+
+        switch (opcion) {
+
+            case 1:
+                System.out.println("selecionaste suma");
+                System.out.println("digita el primer valor");
+
+        }
+
     }
 
 }

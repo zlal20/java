f7296167e5e3f9632150008fb7381ec6b9bc2834
Eduardo Franco <jesuseduardofrancomejia69@gmail.com>
2021-09-09 14:47:44 -0500

    Update notas.java

--- notas.java
+++ notas.java
@@ -13,24 +13,36 @@ public class notas {
         Scanner sc = new Scanner(System.in);
 
         // captura de calificaciones
-        System.out.println("ingresa el promedio de notas 1");
+        System.out.println("ingresa el promedio de nota 1");
         promedio1 = sc.nextDouble();
 
-        System.out.println("ingresa el promedio de notas 1");
+        System.out.println("ingresa el promedio de nota 2");
         promedio2 = sc.nextDouble();
 
-        System.out.println("ingresa el promedio de notas 1");
+        System.out.println("ingresa el promedio de nota 3");
         promedio3 = sc.nextDouble();
 
-        System.out.println("ingresa el promedio de notas 1");
+        System.out.println("ingresa el promedio de nota 4");
         promedio4 = sc.nextDouble();
 
-        System.out.println("ingresa el promedio de notas 1");
+        System.out.println("ingresa el promedio de nota 5");
         promedio5 = sc.nextDouble();
 
         // formula
 
         resultado = (promedio1 + promedio2 + promedio3 + promedio4 + promedio5);
+
+        if (resultado > 7) {
+
+            System.out.println("aprobado");
+            System.out.println("el promedio es " + resultado);
+
+        } else {
+
+            System.out.println("reprobado");
+            System.out.println("el promedio es" + resultado);
+
+        }
 
     }
 

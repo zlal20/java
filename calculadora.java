f278bd2445e7d1cb454eaac20ed9a9d87f5b2a4f
Eduardo Franco <jesuseduardofrancomejia69@gmail.com>
2021-09-09 16:19:56 -0500

    calculadora en java

--- calculadora.java
+++ calculadora.java
@@ -2,7 +2,7 @@
 //simple calculadora en java
 import java.util.Scanner;
 
-public class suma_switch {
+public class calculadora {
 
     public static void main(String[] args) {
 
@@ -10,12 +10,18 @@ public class suma_switch {
         int opcion;
         double numero1, numero2, resultado;
 
-        System.out.println("ingresa un numero del 1 al 2");
+        // menu de opciones
+        System.out.print("1-suma");
+        System.out.print("2-resta");
+        System.out.print("3-multiplicacion");
+        System.out.print("4-division");
+
+        System.out.println("ingresa un numero");
         opcion = sc.nextInt();
 
         switch (opcion) {
 
-            case 0:
+            case 1:
                 System.out.println("selecionaste suma");
                 System.out.println("digita el primer valor");
                 numero1 = sc.nextDouble();
@@ -41,6 +47,33 @@ public class suma_switch {
                 System.out.println("el resultado es" + resultado);
 
                 break;
+
+            case 3:
+                System.out.println("selecionaste multiplicacion");
+                System.out.println("digita el primer valor");
+                numero1 = sc.nextDouble();
+
+                System.out.println("digita el segundo valor");
+                numero2 = sc.nextDouble();
+
+                resultado = (numero1 * numero2);
+                System.out.println("el resultado es" + resultado);
+
+                break;
+
+            case 4:
+                System.out.println("selecionaste division");
+                System.out.println("digita el primer valor");
+                numero1 = sc.nextDouble();
+
+                System.out.println("digita el segundo valor");
+                numero2 = sc.nextDouble();
+
+                resultado = (numero1 / numero2);
+                System.out.println("el resultado es" + resultado);
+
+                break;
+
             default:
                 System.out.println("error debes de ingresar un numero valido");
                 break;
